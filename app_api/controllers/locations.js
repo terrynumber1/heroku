7b88b2867563e26c26b1619f04de20b2b6e6c661
CNNENGRING for tape bar coders <cnnicentral@turner.com>
2016-02-19 07:01:36 -0500

    at page 180, listing 6.3

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -19,13 +19,32 @@ module.exports.locationsCreate = function(req, res) {
 
     sendJSONresponse(res, 200, {"status": "post successful"});
 };
+
+// page 177
 module.exports.locationsReadOne = function(req, res) {
     //sendJSONresponse(res, 200, {"status": "success readone"});
+    // "_id": "56c6e2abf856c2a4aa02d061"
 
-    Loc.findById(req.params.locationid)
-        .exec(function (err, location) {
-            sendJSONresponse(res, 200, location);
-        });
+    if (req.params && req.params.locationid) {
+
+        Loc.findById(req.params.locationid)
+            .exec(function (err, location) {
+                if (!location) {
+                    sendJSONresponse(res, 404, {"message": "locationid not found"});
+                    return; // exit the function
+                } else if (err) {
+                    sendJSONresponse(res, 404, err);
+                    return; // exit the function
+                }
+
+                sendJSONresponse(res, 200, location);
+
+            });
+    } else {
+        sendJSONresponse(res, 404, {"message": "No locationid in request"});
+    }
+
+
 };
 module.exports.locationsUpdateOne = function() {};
 module.exports.locationsDeleteOne = function() {};
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -7,6 +7,68 @@ var sendJSONresponse = function(res, status, content) {
 };
 
 module.exports.reviewsCreate = function() {};
-module.exports.reviewsReadOne = function() {};
+
+// Listing 6.3
+module.exports.reviewsReadOne = function(req, res) {
+
+    // location id 56c6e2abf856c2a4aa02d061
+    // review id 56c6e326f856c2a4aa02d062
+
+    console.log("Getting single review");
+
+    if (req.params && req.params.locationid && req.params.reviewid) {
+        Loc
+            .findById(req.params.locationid)
+            .select('name reviews')
+            .exec(
+            function(err, location) {
+                console.log(location);
+                var response, review;
+                if (!location) {
+                    sendJSONresponse(res, 404, {
+                        "message": "locationid not found"
+                    });
+                    return;
+                } else if (err) {
+                    sendJSONresponse(res, 400, err);
+                    return;
+                }
+                if (location.reviews && location.reviews.length > 0) {
+                    //review = location.reviews.id(req.params.reviewid);
+                    //review = req.params.reviewid;
+                    //review = location.reviews.id;
+
+                    // HERE
+                    console.log('===============: ' + req.params.reviewid);
+                    console.log('REVIEW ID :' + review);
+
+                    if (!review) {
+                        sendJSONresponse(res, 404, {
+                            "message": "reviewid not found"
+                        });
+                    } else {
+                        response = {
+                            location: {
+                                name: location.name,
+                                id: req.params.locationid
+                            },
+                            review: review
+                        };
+                        sendJSONresponse(res, 200, response);
+                    }
+                } else {
+                    sendJSONresponse(res, 404, {
+                        "message": "No reviews found"
+                    });
+                }
+            }
+        );
+    } else {
+        sendJSONresponse(res, 404, {
+            "message": "Not found, locationid and reviewid are both required"
+        });
+    }
+};
+
 module.exports.reviewsUpdateOne = function() {};
 module.exports.reviewsDeleteOne = function() {};
