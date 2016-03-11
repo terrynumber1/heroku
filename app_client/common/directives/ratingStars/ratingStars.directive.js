c67113e49c6df31de54e4dcc807f2421539d74f4
Andy <andy@email.com>
2016-03-11 06:38:39 -0500

    At top of page 310, create new folder pageHeader

--- app_client/common/directives/ratingStars/ratingStars.directive.js
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -3,7 +3,7 @@
   angular
     .module('loc8rApp')
     .directive('ratingStars', ratingStars);
-
+    // <rating-stars></rating-stars>, yeah I know, convert to lowercase letter and add a -
   function ratingStars () {
     return {
       restrict: 'EA',
